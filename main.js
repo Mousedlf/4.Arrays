1f8427e33b6470453302fe4b64c260a2c3c37cba
Mousedlf <dlfcaroline@gmail.com>
2022-10-25 17:47:52 +0200

    encore 1

--- main.js
+++ main.js
@@ -41,20 +41,20 @@ console.log(fullNames)
 // 3. Sort the inventors by birthdate, oldest to youngest
 
 const ordered = inventors.sort((a,b) => a.year>b.year ? 1 : -1)
-/* version courte de:
-    const ordered = inventors.sort (function(a,b){
-        if (a.year>b.year){
-            return 1;
-        } else { return -1;}
-}) */
 console.table(ordered)
+            /* version courte de:
+                const ordered = inventors.sort (function(a,b){
+                    if (a.year>b.year){
+                        return 1;
+                    } else { return -1;}
+            }) */
 
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
 const totalYears = inventors.reduce((total, inventor) => {
     return total + (inventor.passed - inventor.year)
-}, 0) //?
+}, 0) // pourquoi un 0?
 console.log(totalYears)
 
 
@@ -66,12 +66,26 @@ const yearsLived = inventors.sort(function(a,b){
 });
 console.table(yearsLived)
 
+
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+            /* const category = document.querySelector('.mw-category')
+            const links = Array.from(category.querySelectorAll('a')); // || [...categorylalala]
+            const de = links
+                .map(link => link.textContent)
+                .filter(streetName => streetName.includes('de'))
+            console.table(de) */
 
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const alpha = people.sort(function(lastOne,nextOne){
+    const [aLast, aFirst] = lastOne.split(', ') //wut?
+    const [bLast, bFirst] = nextOne.split(', ')
+    return bLast>aLast ? 1 : -1;
+});
+console.table(alpha)
+
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
